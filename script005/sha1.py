7c5bfafc35db216668db676e3b5388cd0684899e
Christian Gutierrez <chesstrian@gmail.com>
2013-05-16 20:14:30 -0500

    Exception for an execution without an argument.

--- script005/sha1.py
+++ script005/sha1.py
@@ -3,4 +3,7 @@
 import sys
 import hashlib
 
-print hashlib.sha1 (sys.argv[1]).hexdigest()
+if len (sys.argv) == 2:
+  print hashlib.sha1 (sys.argv[1]).hexdigest()
+else:
+  print "Usage:\n    " + sys.argv[0] + " arg\n"
