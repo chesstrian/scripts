d22420aa1877713d7784979543c5b8a5e8750938
Christian Gutierrez <chesstrian@gmail.com>
2013-07-10 14:57:35 -0500

    Configuration file for script006.

---
+++ script006/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+// Server 1
+
+$db[0]['host'] = '';
+$db[0]['user'] = '';
+$db[0]['pass'] = '';
+$db[0]['name'] = '';
+
+// Server 2
+
+$db[1]['host'] = '';
+$db[1]['user'] = '';
+$db[1]['pass'] = '';
+$db[1]['name'] = '';
+
+?>
